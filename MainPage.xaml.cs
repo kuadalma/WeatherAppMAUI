7eb8ade6784f26d7f4d4ad494269348e20638df3
kuadalma <adammarzec2808@gmail.com>
2024-04-10 23:38:58 +0200

    add starting Geo & display func

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,13 +7,40 @@ namespace WeatherApp
     public partial class MainPage : ContentPage
     {
         private const string ApiKey = ""; // Wpisz tutaj swój klucz API
-        private const string CityName = "Częstochowa"; // Wpisz tutaj nazwę miasta
+        private string CityName = "Częstochowa"; // Wpisz tutaj nazwę miasta
+        private int Lat = int.MaxValue;
+        private int Lon = int.MaxValue;
+
 
         public MainPage()
         {
             InitializeComponent();
+            GetWeather();
         }
 
+        private async void GetWeather()
+        {
+            if (Lat == int.MaxValue || Lon == int.MaxValue)
+            {
+                var geo = await FeatchGeo(CityName);
+                if (double.TryParse(geo.Lat.ToString(), out _))
+                {
+                    geo.Lat = Lat;
+                    geo.Lon = Lon;
+                }
+            }
+
+            CurrentWeatherModel CWeather = await FetchCurrentWeather(Lat.ToString(), Lon.ToString());
+            if (int.TryParse(CWeather.Cod.ToString(), out int statusCode) && statusCode >= 200 && statusCode < 300)
+            {
+                DisplayCurrentWeather(CWeather);
+            }
+        }
+
+        private void DisplayCurrentWeather(CurrentWeatherModel weather)
+        {
+            _ = 0;
+        }
         
         private static async Task<GeoModel> FeatchGeo(string cityName)
         {
@@ -31,8 +58,7 @@ namespace WeatherApp
             ForecastWeatherModel ForecastWeather = JsonConvert.DeserializeObject<ForecastWeatherModel>(json);
             return ForecastWeather;
         }
-
-        private static async Task<CurrentWeatherModel> FetchCurentWeather(string Lat, string Lon)
+        private static async Task<CurrentWeatherModel> FetchCurrentWeather(string Lat, string Lon)
         {
             using var client = new HttpClient();
             var res = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={Lat}&lon={Lon}&appid={ApiKey}");
