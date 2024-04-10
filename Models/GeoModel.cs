afec240586122270b8a982761190102d147b936e
kuadalma <adammarzec2808@gmail.com>
2024-04-10 23:44:34 +0200

    modify Geo Model

--- Models/GeoModel.cs
+++ Models/GeoModel.cs
@@ -5,5 +5,7 @@
         public string Name { get; set; }
         public double Lat { get; set; }
         public double Lon { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
     }
 }
