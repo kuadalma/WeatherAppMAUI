dc3a46bc425af403775da622c1e761e42d23b24c
kuadalma <adammarzec2808@gmail.com>
2024-04-10 22:12:53 +0200

    add API data fetching

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,44 @@
-﻿namespace WeatherApp
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WeatherApp.Models;
+
+namespace WeatherApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private const string ApiKey = ""; // Wpisz tutaj swój klucz API
+        private const string CityName = "Częstochowa"; // Wpisz tutaj nazwę miasta
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        
+        private static async Task<GeoModel> FeatchGeo(string cityName)
         {
-            count++;
+            using var client = new HttpClient();
+            var res = await client.GetAsync($"http://api.openweathermap.org/geo/1.0/direct?q={cityName}&appid={ApiKey}");
+            var json = await res.Content.ReadAsStringAsync();
+            GeoModel Geo = JsonConvert.DeserializeObject<GeoModel>(json);
+            return Geo;
+        }
+        private static async Task<ForecastWeatherModel> FetchForcastWeather(string Lat, string Lon)
+        {
+            using var client = new HttpClient();
+            var res = await client.GetAsync($"http://api.openweathermap.org/data/2.5/forecast?lat={Lat}&lon={Lon}&units=metric&appid={ApiKey}");
+            var json = await res.Content.ReadAsStringAsync();
+            ForecastWeatherModel ForecastWeather = JsonConvert.DeserializeObject<ForecastWeatherModel>(json);
+            return ForecastWeather;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private static async Task<CurrentWeatherModel> FetchCurentWeather(string cityName)
+        {
+            using var client = new HttpClient();
+            var res = await client.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid={ApiKey}");
+            var json = await res.Content.ReadAsStringAsync();
+            CurrentWeatherModel CurrentWeather = JsonConvert.DeserializeObject<CurrentWeatherModel>(json);
+            return CurrentWeather;
         }
     }
 
